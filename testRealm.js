89600ddddbfe725e5aaccc8dc0898a6b7dfd1db7
Thomas <Thomas@Thomas-Mac.local>
2017-06-27 10:44:33 -0600

    first commit

--- testRealm.js
+++ testRealm.js
@@ -1,3 +1,25 @@
 const gtest = require('https://maps.googleapis.com/maps/api/js?key=AIzaSyCr6EwvdOcCpVEe5Xir9aLi_neVGEu6THA')
 
 console.log(navigator.geolocation.getCurrentPosition(gtest));
+
+let headers = {
+  Accept: "application/json",
+  user-key: "b3549408bdd1a9da0380f2f2aaf4efa6"
+}
+
+response.headers["https://developers.zomato.com/api/v2.1/cities?lat=46.86&lon=-113.97"] = request.url
+
+console.log(HERP);
+
+
+
+//curl -X GET --header "Accept: application/json" --header "user-key: b3549408bdd1a9da0380f2f2aaf4efa6"
+//"https://developers.zomato.com/api/v2.1/cities?lat=46.86&lon=-113.97"
+
+
+
+
+["https://developers.zomato.com/api/v2.1/cities?lat="+(latitiude)+"&lon="+(longitude)]
+
+var latitude = 46.86 //this will be code that pulls number from geolocation
+var longitude = -113.97 //this will be code that pulls number from geolocation
