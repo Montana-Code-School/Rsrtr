ce81870f6ef7238c1d1ba0341748ee7edf3a41b2
MacUser <macuser@MacUsers-MacBook-Pro.local>
2017-06-27 14:25:43 -0600

    fetch work

--- src/quinnTestReq.js
+++ src/quinnTestReq.js
@@ -2,9 +2,24 @@ import React from ('react');
 
 export default class Quinntest extends React.Component {
   state = {isLoading: true}
+
+
   render(){
     result (
-      
+
     )
   }
 }
+
+
+/*fetch('https://mywebsite.com/endpoint/', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+    secondParam: 'yourOtherValue',
+  })
+})
