854e08800fa7348c3d67fb01e9819f68a12ff0a6
MacUser <macuser@MacUsers-MacBook-Pro.local>
2017-06-28 11:08:55 -0600

    An Array of names

--- src/quinnTestReq.js
+++ src/quinnTestReq.js
@@ -1,36 +1,54 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch'
 
+let lat;
+let lng;
+
+
 export default class Quinntest extends Component {
+  state = {
+    lat: null,
+    lng: null
+  }
 
   componentDidMount() {
-   fetch('https://developers.zomato.com/api/v2.1/cities?lat=46.86&lon=-113.97', {
-      //method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'user-key': 'b3549408bdd1a9da0380f2f2aaf4efa6'
-      }
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      // this.setState({
-      //   isLoading: false,
-      //   dataSource:responseJson.movies
-      // });
-      console.log(responseJson)
-    }).catch((error) => {
-      console.error(error);
-    });
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+
+        lat = position.coords.latitude,
+        lng = position.coords.longitude;
+        this.setState({lat: lat, lng: lng},  ()=>{
+          fetch(`https://developers.zomato.com/api/v2.1/search?lat=${this.state.lat}&lon=${this.state.lng}`, {
+            //method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'user-key': 'b3549408bdd1a9da0380f2f2aaf4efa6'
+            }
+          })
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log(responseJson);
+            console.log(responseJson.restaurants.map((obj)=> obj.restaurant.name))
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        })
+      });
+    } else {
+      console.log('Oh snap')
+    }
+
   }
 
   render() {
 
-
     return (
-      <div> HAI
-    </div>
+      <div>
+        {this.state.lat ? "You are located at " + this.state.lat + " , " + this.state.lng : "No location found"}
+      </div>
     );
   }
 }
 
-//my late night guess, is this is not going to maintain a react component, but rather will become a backend server function request (thingy) when the uses hits the submit button on filters (which would change the url in the fetch.). 
+//my late night guess, is this is not going to maintain a react component, but rather will become a backend server function request (thingy) when the uses hits the submit button on filters (which would change the url in the fetch.).
